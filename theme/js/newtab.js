3f22499a9ae5a3b9761963d34d724dc22911f5ea
Phuker <1403490607@qq.com>
2017-06-17 00:53:55 +0800

    modification

--- theme/js/newtab.js
+++ theme/js/newtab.js
@@ -1,5 +1,9 @@
 (function(){
 	var div = document.getElementsByClassName('entry-content');
+	var featured = document.getElementById('featured');
+	if(featured !== null){  // not in a article
+		return;
+	}
 	if(div.length > 0){
 		div = div[0];
 		var links = div.getElementsByTagName('a');
